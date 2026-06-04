9e825b75d8d39573dbfcd7b5f29e6bd93efdf02e
Dao Duy Quyen <quyenne3704@gmail.com>
2026-06-04 11:29:29 +0700

    update: ver 1

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,10 @@
 import { defineConfig } from "vite";
 import viteReact from "@vitejs/plugin-react";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [tanstackStart(), viteReact(), tsconfigPaths(), tailwindcss()],
-  server: {
-    strictPort: true,
-  },
+  plugins: [tanstackStart(), nitro(), viteReact(), tsconfigPaths(), tailwindcss()],
 });
-
